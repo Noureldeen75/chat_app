b5cbfbd248c00d509d7e52771a32c663549fe7bf
Nour <nour70x@gmail.com>
2023-07-08 15:28:34 +0300

    Adding TextStyles class and YsabeauOffice font

---
+++ lib/core/utils/text_styles/TextStyles.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyles {
+  static const textSize30 =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w600, fontFamily: 'YsabeauOffice');
+}
--- lib/features/Auth/presentation/views/login/widgets/login_view_body.dart
+++ lib/features/Auth/presentation/views/login/widgets/login_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/core/utils/text_styles/TextStyles.dart';
+import 'package:chat_app/core/widgets/custom_background.dart';
 import 'package:flutter/material.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -5,10 +7,12 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        
-      ],
+    return CustomBackground(
+      child: Column(
+        children: [
+          Text("WhyApp", style: TextStyles.textSize30,)
+        ],
+      ),
     );
   }
 }
