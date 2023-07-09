8c39395605c3ac9068678f046fee593c8fafea05
Nour <nour70x@gmail.com>
2023-07-09 15:53:59 +0300

    linking the application with the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,14 @@ import 'package:chat_app/features/Auth/presentation/views/login/login_view.dart'
 import 'package:chat_app/features/Auth/presentation/views/register/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
